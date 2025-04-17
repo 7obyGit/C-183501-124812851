1f77941dcfea1a0cfa9baf3131e56ac104da37ea
7obyGit <7obyGitt@gmail.com>
2025-04-17 18:47:06 +0100

    Updated auto-generated files

--- cc2/src/entrypoints/example/update.lua
+++ cc2/src/entrypoints/example/update.lua
@@ -6,6 +6,7 @@ baseUrl = "https://raw.githubusercontent.com/7obyGit/C-183501-124812851/main/cc2
 
 
 function download(url)
+    print(url)
     local request = http.get(url)
     local text = request.readAll()
     text = (text:sub(1,1) == "?" and text:sub(2)) or text
