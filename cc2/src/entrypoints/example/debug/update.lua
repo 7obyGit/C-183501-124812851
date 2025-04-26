5c28952f1f37a61a85675e8a035fba621f7f5444
7obyGit <7obyGitt@gmail.com>
2025-04-26 11:15:06 +0100

    Updated auto-generated files

--- cc2/src/entrypoints/example/debug/update.lua
+++ cc2/src/entrypoints/example/debug/update.lua
@@ -2,7 +2,7 @@
 --- It will only update everything if there is a new version of the app to be installed
 
 
-baseUrl = "https://raw.githubusercontent.com/7obyGit/C-183501-124812851/main/cc2/src/entrypoints/example/debug"
+baseUrl = "https://raw.githubusercontent.com/7obyGit/C-183501-124812851/main/cc2/src/entrypoints/example//debug"
 
 
 function download(url)
