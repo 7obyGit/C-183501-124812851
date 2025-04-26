8afb4abb2578654a6b4eab2b9317f79bfa5478f9
7obyGit <7obyGitt@gmail.com>
2025-04-26 15:49:21 +0100

    Updated auto-generated files

--- cc2/src/entrypoints/example/debug/app/main.lua
+++ cc2/src/entrypoints/example/debug/app/main.lua
@@ -1,4 +1,30 @@
---- Gather the command line arguments to ensure they are accessible later
+local function require(file, ...)
+    if ____moduleCache[file] then
+        return ____moduleCache[file].value
+    end
+
+    if ____modules[file] then
+        local module = ____modules[file]
+
+        ____moduleCache[file] = { value = {} }
+
+        local value
+        if select("#", ...) > 0 then
+            value = module(...)
+        else
+            value = module(file)
+        end
+
+        ____moduleCache[file].value = value
+        return value
+    else
+        if ____originalRequire then
+            return ____originalRequire(file)
+        else
+            error("module '" .. file .. "' not found")
+        end
+    end
+end--- Gather the command line arguments to ensure they are accessible later
 --- This is necessary because the rest of the program is inside of transpiled functions and therefore `{...}`
 --- would not return command line arguments when used elsewhere
 COMMAND_LINE_ARGUMENTS = {...}
