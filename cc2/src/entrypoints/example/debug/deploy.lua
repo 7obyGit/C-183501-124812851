39805227740e15ad3eec651d22761aed52144bcd
7obyGit <7obyGitt@gmail.com>
2025-04-26 11:15:24 +0100

    Updated auto-generated files

--- cc2/src/entrypoints/example/debug/deploy.lua
+++ cc2/src/entrypoints/example/debug/deploy.lua
@@ -3,7 +3,7 @@
 --- be easily copied into a computer
 
 
-baseUrl = "https://raw.githubusercontent.com/7obyGit/C-183501-124812851/main/cc2/src/entrypoints/example//debug"
+baseUrl = "https://raw.githubusercontent.com/7obyGit/C-183501-124812851/main/cc2/src/entrypoints/example/debug"
 
 
 function download(url)
