83ac4fa1de945913c42478e8237f2a8091e5ff6a
7obyGit <7obyGitt@gmail.com>
2025-04-27 14:16:51 +0100

    Updated auto-generated files

--- cc2/src/entrypoints/example/debug/app/main.lua
+++ cc2/src/entrypoints/example/debug/app/main.lua
@@ -52,7 +52,7 @@ v8)0;*5/0v+).9,L9-)5,9u
 2/;=2~??JK??[2=++Y&*90:+~a~????2)=25<p??JK??[2=++Y&*90:+
 2/;=2~??JK??P9'~a~????2)=25<p??JK??P9'
 2/;=2~??JK??K/),;9Q=.J,=;9\=;3~a~????2)=25<p??JK??K/),;9Q=.J,=;9\=;3
-??JK??K/),;9Q=.J,=;9\=;3v:9<)7p79*508/vmup+6/,*?+,;r~#C|f|A~a~mrC|e|A~a~mrC|mn|A~a~lrC|mm|A~a~lrC|ml|A~a~krC|mk|A~a~krC|mj|A~a~jrC|mi|A~a~jrC|mh|A~a~hrC|mg|A~a~hrC|mf|A~a~hrC|me|A~a~hrC|ln|A~a~grC|lm|A~a~grC|ll|A~a~frC|lk|A~a~frC|lj|A~a~mnrC|li|A~a~mmrC|lh|A~a~mlrC|lg|A~a~mkrC|lf|A~a~mjrC|le|A~a~mirC|kn|A~a~mnrC|km|A~a~me!uc
+??JK??K/),;9Q=.J,=;9\=;3v:9<)7p79*508/vmup+6/,*?+,;r~#C|f|A~a~mrC|e|A~a~mrC|mn|A~a~lrC|mm|A~a~lrC|ml|A~a~krC|mk|A~a~krC|mj|A~a~jrC|mi|A~a~jrC|mh|A~a~hrC|mg|A~a~hrC|mf|A~a~hrC|me|A~a~hrC|ln|A~a~grC|lm|A~a~grC|ll|A~a~frC|lk|A~a~frC|lj|A~a~mnrC|li|A~a~mmrC|lh|A~a~mlrC|lg|A~a~mkrC|lf|A~a~mjrC|le|A~a~mirC|km|A~a~mgrC|kl|A~a~mgrC|kk|A~a~mfrC|kj|A~a~mgrC|kg|A~a~mnrC|kf|A~a~lh!uc
 2/;=2~????9&./,*+~a~#!
 2/;=2~????;6=*\/&~a~,9-)5,9v|+,;p)*52p;/1.)*9,p=.5p*59,mp=:(=0;9:N9,5.69,=2+p;6=*\/&|u
 2/;=2~[6=*\/&~a~????;6=*\/&p[6=*\/&
@@ -76,6 +76,13 @@ Y&=1.29Y0*,%./50*p0=19~a~|Y&=1.29Y0*,%./50*|
 ~~~~R/779,d:9<)7v25+*u
 ~~~~[6=*\/&d+90:Q9++=79v|V922/~G/,2:}|u
 ~~~~R/779,d'=,0v|R=+*|u
+~~~~:/
+~~~~~~~~2/;=2~5~a~n
+~~~~~~~~'6529~5~b~mnnn~:/
+~~~~~~~~~~~~R/779,d:9<)7v|J9+*507~=~2/07~19++=79~*/~+99~58~5*~',=.+~=0:~5+~6=0:29:~;/,,9;*2%~<%~*69~2/779,~|~pp~*/+*,507v5uu
+~~~~~~~~~~~~5~a~5~s~m
+~~~~~~~~90:
+~~~~90:
 90:
 ??JK??P9'v????9&./,*+pY&=1.29Y0*,%./50*ud,)0vu
 ,9*),0~????9&./,*+
